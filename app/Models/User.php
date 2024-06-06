74dac2ad76677168f6a828026e79395608300be4
Daniel <danielheinze@MacBook-Air-Daniel.local>
2024-06-06 11:46:49 +0200

    Determinate User Access to Panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -44,4 +45,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * By default all users have access to Panel
+     *
+     */
+//    public function canAccessPanel(Panel $panel): bool
+//    {
+//        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+//    }
 }
